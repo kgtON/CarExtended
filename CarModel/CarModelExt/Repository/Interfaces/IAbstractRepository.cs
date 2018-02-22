88f16c3f90fd441db190f55f86d79302600ce4e2
DESKTOP-4HJ1KAG\Admin <radek.db@gmail.com>
2018-02-22 18:21:02 +0100

    +CarExtended

---
+++ CarModel/CarModelExt/Repository/Interfaces/IAbstractRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using CarModelExt.Repository.Interfaces;
+
+namespace CarModelExt.Repository.Interfaces
+{
+    public interface IAbstractRepository<T> where T : class, IBasicEntity
+    {
+        void Create(T entity);
+        void Delete(T entity);
+        void Edit(T entity);
+        List<T> GetWhere(Expression<Func<T, bool>> expression);
+    }
+}
