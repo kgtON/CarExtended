bb1f8242576dfeba634b775a42c65819810c9d35
rdab <radek.db@gmail.com>
2018-03-02 01:18:27 +0100

    Added WebApi

---
+++ CarModel/CarModelExtWebApi/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace CarModelExtWebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
